13eb091ed51e408f8c8b5744e451c1085c647152
ronyislam502 <ronyislam502@gmail.com>
2024-11-10 15:15:34 +0600

    upvote

--- src/components/module/articles/UpVote.tsx
+++ src/components/module/articles/UpVote.tsx
@@ -1,17 +1,20 @@
 "use client";
-import { TUser, useCurrentUser } from "@/src/redux/features/auth/authSlice";
-import { useUpvoteMutation } from "@/src/redux/features/vote";
-import { useAppSelector } from "@/src/redux/hooks";
-import { TErrorResponse } from "@/src/types";
-import { FaAnglesUp } from "react-icons/fa6";
+
 import { toast } from "sonner";
+import { FaAnglesUp } from "react-icons/fa6";
+
+import { selectCurrentUser, TUser } from "@/src/redux/features/auth/authSlice";
+import { useUpVotesPostMutation } from "@/src/redux/features/vote/voteApi";
+import { useAppSelector } from "@/src/redux/hooks";
+import { TError } from "@/src/types/global";
+
 export type TVoteProps = {
   votes: string[];
   id: string;
 };
 const UpVote = ({ votes, id }: TVoteProps) => {
-  const user = useAppSelector(useCurrentUser) as TUser;
-  const [createVote] = useUpvoteMutation();
+  const user = useAppSelector(selectCurrentUser) as unknown as TUser;
+  const [createVote] = useUpVotesPostMutation();
 
   // Check if the current user has already upvoted
   const hasVoted = user ? votes?.includes(user?.id) : false;
@@ -19,24 +22,29 @@ const UpVote = ({ votes, id }: TVoteProps) => {
   const handleUpVote = async (id: string) => {
     if (!user) {
       toast.error("You need to log in first!");
+
       return;
     }
-    const toastId = toast.loading("Upvote  posting");
+    const toastId = toast.loading("UpVote  posting");
+
     try {
       const res = await createVote(id).unwrap();
+
       toast.success(res.message, { id: toastId, duration: 2000 });
     } catch (error) {
-      const err = error as TErrorResponse;
-      toast.error(err?.data?.errorMessages[0].message, {
+      const err = error as TError;
+
+      toast.error(err?.data?.message, {
         id: toastId,
         duration: 2000,
       });
     }
   };
+
   return (
     <button
-      onClick={() => handleUpVote(id)}
       className="flex items-center justify-center gap-2 p-2 w-full"
+      onClick={() => handleUpVote(id)}
     >
       <FaAnglesUp className={`${hasVoted ? "text-primary" : ""}`} />{" "}
       {votes?.length}
